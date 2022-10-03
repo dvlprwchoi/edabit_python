60b0ad1784032b74ae65b74d0d9c6e26dca8bfa5
dvlprwchoi <dvlpr.wchoi@gmail.com>
2022-10-03 11:41:35 -0500

    Add is the number even or odd

---
+++ is_the_number_even_or_odd/is_the_number_even_or_odd.py
@@ -0,0 +1,9 @@
+def isEvenOrOdd(num):
+    if num % 2 == 0:
+        return 'even'
+    else:
+        return 'odd'
+
+
+def isEvenOrOdd(num):
+    return 'odd' if num % 2 else 'even'
--- is_the_word_singular_or_plural/is_the_word_singular_or_plural.py
+++ is_the_word_singular_or_plural/is_the_word_singular_or_plural.py
@@ -1,2 +1,6 @@
 def is_plural(word):
     return word[-1] == 's'
+
+
+def is_plural(word):
+    return word.endswith('s')
